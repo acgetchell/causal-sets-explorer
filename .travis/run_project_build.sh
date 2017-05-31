1fc3119afa58636adb17f52d0fd5530fab41c61d
Luis Martinez de Bartolome <lasote@gmail.com>
2017-05-31 11:11:26 +0200

    try now

--- .travis/run_project_build.sh
+++ .travis/run_project_build.sh
@@ -4,3 +4,7 @@ rm -rf build && mkdir -p build && cd build
 conan install ../ --build=missing
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
+
+ls -la build/bin
+cd build/bin && ./causal_sets_explorer
+cd build/bin && ./unittests
