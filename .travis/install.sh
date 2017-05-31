59773841ab83184ffffdb974d1dde36bb9c969b8
Luis Martinez de Bartolome <lasote@gmail.com>
2017-05-31 11:25:48 +0200

    conan brew

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,17 +11,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew upgrade cmake
     brew install gcc || brew link --overwrite gcc
     brew install ninja
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
-    pyenv rehash
-    pyenv activate conan
-
-    pip install conan
+    brew install conan
     conan user
-
 fi
