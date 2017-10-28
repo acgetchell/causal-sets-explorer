8fbc2dc74de1cd6b5d32e1c649f8d7d8d0e834f6
Adam Getchell <acgetchell@ucdavis.edu>
2017-10-27 19:59:57 -0700

    First crack at conan installing cmake
    New version of conan should also grok Clang-5

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class CausalSetsExplorer(ConanFile):
     requires = "Boost.Graph/1.65.1@bincrafters/stable", "gmock/1.8.0@meshell/stable"
     generators = "cmake"
     # default_options = "Boost:header_only=True"
+    build_requires = "cmake_installer/1.0@conan/stable"
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
@@ -15,3 +16,6 @@ class CausalSetsExplorer(ConanFile):
         cmake.verbose = True
         cmake.configure(args=["CMAKE_BUILD_TYPE=Release", "GMOCK_TESTS:BOOL=ON"])
         cmake.build()
+
+    def build_requirements(self):
+        self.options.version = "3.9.0"
